41ad7dadb37e72d5230808fafca4beab4c6f107b
Naadira Sahar N <naadira@IND-71>
2025-02-27 13:27:16 +0530

    Fix the deployment

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-streamlit run sample.py --server.port=8000 --server.address=0.0.0.0
+pip install -r requirements.txt  # Install dependencies
+streamlit run app.py --server.port=8000 --server.address=0.0.0.0
+
